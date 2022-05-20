45c8c478f50711ed7abd23926572bd8b8b78d0a0
Rob Maskell <robmaskell@gmail.com>
2022-05-20 22:44:24 +0100

    add railgun and fix weapon table spacing

--- js/defaultToon_rewrite.js
+++ js/defaultToon_rewrite.js
@@ -244,7 +244,8 @@ var defaultToon = {
         { 'gear': 'Mod: Medical Repair',	'complexity': 'Min/1', 'gp-cost': 1, 'desc': 'Heals 1d10 damage per hour, ignore 1 wound', 'src': '', 'qty': 1, 'energy': 0, 'kinetic': 0 }
     ],
     'weapons': [
-        { 'gear': 'Medium Pistol Firearm',	'complexity': 'Min/R/1', 'gp-cost': 0, 'desc': 'DV 2d10, SA/BF/FA, Ammo 15+1, Range 30', 'src': 'Firewall Pack', 'qty': 0, 'dmg': '2d10', 'modes': 'SA/BF/FA', 'ammo': '15+1', 'range': '30' }
+        { 'gear': 'Medium Pistol Firearm',	'complexity': 'Min/R/1', 'gp-cost': 0, 'desc': 'DV 2d10, SA/BF/FA, Ammo 15+1, Range 30', 'src': 'Firewall Pack', 'qty': 0, 'dmg': '2d10', 'modes': 'SA/BF/FA', 'ammo': '15+1', 'range': '30' },
+        { 'gear': 'Medium Pistol Railgun',	'complexity': 'Min/R/1', 'gp-cost': 0, 'desc': 'DV 2d10, SA/BF/FA, Ammo 30+1, Range 45', 'src': 'Picked Up', 'qty': 0, 'dmg': '2d10', 'modes': 'SA/BF/FA', 'ammo': '30+1', 'range': '30' }
     ],
     'morph': {
         'name': 'Bouncer',
