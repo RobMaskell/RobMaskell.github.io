5790cc4f7f0fba774b181ef42829955e16216645
Rob Maskell <robmaskell@gmail.com>
2023-02-25 22:10:57 +0000

    Update defaultToon.js

--- js/defaultToon.js
+++ js/defaultToon.js
@@ -277,7 +277,7 @@ var defaultToon = {
             'trait': 'Adaptability',
             'desc': 'Easier resleeving +10 to resleeving and integration',
             'cp-adj': 0,
-            'rp-adj': -2
+            'rp-adj': 2
         },
         {
             'trait': 'Restricted Behaviour',
